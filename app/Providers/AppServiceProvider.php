ca8b86f6e3aa528e7dcaf6dd65dbdf1256c87965
Tomu <fantasista2150@outlool.jp>
2024-06-06 13:06:38 +0900

    fix security problem

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,7 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
-
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
 
         Gate::define('admin', function ($user) {
             return $user->role_id == User::ADMIN_ROLE_ID;
